b6d63b4424b9668469c3d1b1a7c6b59abe81dd2d
Lucas Dang <dhp371.work@gmail.com>
2024-02-06 22:20:19 +0700

    feat: change keycloak bitnami to official image

--- postgresql/init-db.sh
+++ postgresql/init-db.sh
@@ -7,7 +7,7 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-E
 	GRANT ALL PRIVILEGES ON DATABASE "kma_score" TO "kma_score";
     ALTER DATABASE "kma_score" OWNER TO "kma_score";
 
-    CREATE USER "keycloak" WITH PASSWORD '$KEYCLOAK_DATABASE_PASSWORD';
+    CREATE USER "keycloak" WITH PASSWORD '$KC_DB_PASSWORD';
 	CREATE DATABASE "keycloak";
 	GRANT ALL PRIVILEGES ON DATABASE "keycloak" TO "keycloak";
     ALTER DATABASE "keycloak" OWNER TO "keycloak";
